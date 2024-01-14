3c4545cabc85e0b6926b3689fe1f77e68cca6df1
Katerina Zakova <zakova.kz@gmail.com>
2024-01-14 23:36:46 +0100

    fix (Main.java): fix block try-catch in while loop

--- src/main/java/com/github/katerinazakova/Main.java
+++ src/main/java/com/github/katerinazakova/Main.java
@@ -11,29 +11,32 @@ public class Main {
             try {
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("Enter coordinates");
-                int column = scanner.nextInt();
-                int row = scanner.nextInt();
+                if (scanner.hasNextInt()) {
+                    int column = scanner.nextInt();
+                    int row = scanner.nextInt();
 
 
-                if (isNotValidCoordinates(row, column)) {
-                    System.out.println("Print coordinates should be from 1 to 3!");
+                    if (isNotValidCoordinates(row, column)) {
+                        System.out.println("Print coordinates should be from 1 to 3!");
 
-                } else {
-                    GameCells.takeCell(row, column, currentPlayer);
-                    GameBoard.printGameBoard();
-                    if (Win.isPlayerVictory(currentPlayer)) {
-                        System.out.println("Status game: " + currentPlayer + " wins");
-                        return;
-                    } else if (GameCells.isOccupiedAllCells()) {
-                        System.out.println("Status game: draw");
-                        return;
+                    } else {
+                        GameCells.takeCell(row, column, currentPlayer);
+                        GameBoard.printGameBoard();
+                        if (Win.isPlayerVictory(currentPlayer)) {
+                            System.out.println("Status game: " + currentPlayer + " wins");
+                            return;
+                        } else if (GameCells.isOccupiedAllCells()) {
+                            System.out.println("Status game: draw");
+                            return;
+                        }
+                        System.out.println("Game not finished");
+                        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
                     }
-                    System.out.println("Game not finished");
-                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                } else {
+                    System.out.println("You should enter numbers!");
                 }
-
             } catch (NumberFormatException e) {
-                System.out.println("You should enter numbers!");
+                System.out.println("Errors" + e.getMessage());
             }
 
         }
