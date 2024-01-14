fa430e274c476b1bc8455ec93424313f0c1e8f86
Katerina Zakova <zakova.kz@gmail.com>
2024-01-14 15:55:47 +0100

    refactor (Main.java): refactor while loop

--- src/main/java/com/github/katerinazakova/Main.java
+++ src/main/java/com/github/katerinazakova/Main.java
@@ -2,71 +2,51 @@ package com.github.katerinazakova;
 
 import java.util.Scanner;
 
-import static com.github.katerinazakova.GameBoard.makeEmptyGameBoard;
-import static com.github.katerinazakova.GameBoard.printGameBoard;
-import static com.github.katerinazakova.Player.*;
+import static com.github.katerinazakova.GameBoard.printEmptyGameBoard;
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
         int numberOfRows = 3;
         int numberOfColumns = 5;
         char[][] ticTacToeArray = new char[numberOfRows][numberOfColumns];
-        makeEmptyGameBoard(ticTacToeArray, numberOfRows, numberOfColumns);
+        printEmptyGameBoard(ticTacToeArray, numberOfRows, numberOfColumns);
         char currentPlayer = 'X';
 
         while (true) {
-            System.out.println("Enter coordinates:");
-            String coordinateNumberOne = scanner.next();
-            String coordinateNumberTwo = scanner.next();
-
             try {
-                int firstCoordinate = Integer.parseInt(coordinateNumberOne);
-                int secondCoordinate = Integer.parseInt(coordinateNumberTwo);
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter coordinates");
+                int column = scanner.nextInt();
+                int row = scanner.nextInt();
 
-                if (firstCoordinate < 1 || firstCoordinate > 3 || secondCoordinate < 1 || secondCoordinate > 3) {
+                if (isNotValidCoordinates(row, column)) {
                     System.out.println("Print coordinates should be from 1 to 3!");
+
                 } else {
-                    String result;
-                    if (currentPlayer == 'X') {
-                        result = controlPlaceForPlayerX(ticTacToeArray, firstCoordinate, secondCoordinate);
-                    } else {
-                        result = controlPlaceForPlayerO(ticTacToeArray, firstCoordinate, secondCoordinate);
+                    GameCells.takeCell(ticTacToeArray, row, column, currentPlayer);
+                    GameBoard.printGameBoard(ticTacToeArray);
+                    if (Win.isPlayerVictory(ticTacToeArray, currentPlayer)) {
+                        System.out.println("Status game: " + currentPlayer + " wins");
+                        return;
+                    } else if (GameCells.isOccupiedAllCells(ticTacToeArray)) {
+                        System.out.println("Status game: draw");
+                        return;
                     }
-
-                    if (result.equals("This cell was changed to " + currentPlayer)) {
-                        printGameBoard(ticTacToeArray);
-                        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-
-                    } else {
-                        System.out.println("This cell is occupied! Choose another one!");
-                    }
+                    System.out.println("Game not finished");
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
                 }
+
             } catch (NumberFormatException e) {
                 System.out.println("You should enter numbers!");
             }
-            String xWins = controlToPlayerX(ticTacToeArray);
-            String oWins = controlToPlayerO(ticTacToeArray);
-            String emptyPlace = controlToEmptyPlace(ticTacToeArray);
-            String numberXAndO = controlToNumberXAndNumberO(ticTacToeArray);
-            if (xWins.equals("X wins") && oWins.equals("O wins")
-                    || numberXAndO.equals("Impossible")) {
-                System.out.println("Impossible");
-            } else if (xWins.equals("X wins")) {
-                System.out.println(xWins);
-                return;
-            } else if (oWins.equals("O wins")) {
-                System.out.println(oWins);
-                return;
-            } else if (emptyPlace.equals("Empty place exist")) {
-                System.out.println("Game not finished");
-            } else {
-                System.out.println("Draw");
-                return;
-            }
+
         }
+
     }
 
+    public static boolean isNotValidCoordinates(int row, int column) {
+        return (row < 1 || row > 3 || column < 1 || column > 3);
+
+    }
 
 }
