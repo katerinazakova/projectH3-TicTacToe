447f891c63a0491a3a899f982beddad204107551
Katerina Zakova <zakova.kz@gmail.com>
2024-01-15 09:11:27 +0100

    fix (Main.java): fix block try-catch in while loop

--- src/main/java/com/github/katerinazakova/Main.java
+++ src/main/java/com/github/katerinazakova/Main.java
@@ -11,41 +11,30 @@ public class Main {
             try {
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("Enter coordinates");
-                if (scanner.hasNextInt()) {
-                    int column = scanner.nextInt();
-                    int row = scanner.nextInt();
+                int column = scanner.nextInt();
+                int row = scanner.nextInt();
 
+                if (AnalyseGame.isNotValidCoordinates(row, column)) {
+                    System.out.println("Print coordinates should be from 1 to 3!");
 
-                    if (isNotValidCoordinates(row, column)) {
-                        System.out.println("Print coordinates should be from 1 to 3!");
-
-                    } else {
-                        GameCells.takeCell(row, column, currentPlayer);
-                        GameBoard.printGameBoard();
-                        if (Win.isPlayerVictory(currentPlayer)) {
-                            System.out.println("Status game: " + currentPlayer + " wins");
-                            return;
-                        } else if (GameCells.isOccupiedAllCells()) {
-                            System.out.println("Status game: draw");
-                            return;
-                        }
-                        System.out.println("Game not finished");
-                        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-                    }
                 } else {
-                    System.out.println("You should enter numbers!");
+                    GameCells.takeCell(row, column, currentPlayer);
+                    GameBoard.printGameBoard();
+                    if (Win.isPlayerVictory(currentPlayer)) {
+                        System.out.println("Status game: " + currentPlayer + " wins");
+                        return;
+                    } else if (GameCells.isOccupiedAllCells()) {
+                        System.out.println("Status game: draw");
+                        return;
+                    }
+                    System.out.println("Game not finished");
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
                 }
-            } catch (NumberFormatException e) {
-                System.out.println("Errors" + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("You should enter numbers!");
             }
 
         }
 
     }
-
-    public static boolean isNotValidCoordinates(int row, int column) {
-        return (row < 1 || row > 3 || column < 1 || column > 3);
-
-    }
-
 }
