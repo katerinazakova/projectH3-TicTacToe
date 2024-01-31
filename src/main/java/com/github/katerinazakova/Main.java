75cedbf8de887c0d2897c326303d3e5eb90064fa
Katerina Zakova <zakova.kz@gmail.com>
2024-01-31 10:30:46 +0100

    fix(Main.java): fix while loop

--- src/main/java/com/github/katerinazakova/Main.java
+++ src/main/java/com/github/katerinazakova/Main.java
@@ -11,27 +11,26 @@ public class Main {
             try {
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("Enter coordinates");
-                int column = scanner.nextInt();
                 int row = scanner.nextInt();
+                int column = scanner.nextInt();
 
-                if (AnalyseGame.isNotValidCoordinates(row, column)) {
-                    System.out.println("Print coordinates should be from 1 to 3!");
-
-                } else {
-                    GameCells.takeCell(row, column, currentPlayer);
-                    GameBoard.printGameBoard();
-                    if (Win.isPlayerVictory(currentPlayer)) {
+                if (("This cell was changed to " + currentPlayer).equals(GameCells.takeCell(row, column, currentPlayer))) {
+                    GameBoard.printActualGameBoard();
+                    if (GameAnalysis.isPlayerVictory(currentPlayer)) {
                         System.out.println("Status game: " + currentPlayer + " wins");
                         return;
-                    } else if (GameCells.isOccupiedAllCells()) {
+                    } else if (GameAnalysis.isOccupiedAllCells()) {
                         System.out.println("Status game: draw");
                         return;
+                    } else {
+                        System.out.println("Game not finished");
+                        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
                     }
-                    System.out.println("Game not finished");
-                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                } else {
+                    System.out.println("This cell is occupied! Choose another one!");
                 }
             } catch (Exception e) {
-                System.out.println("You should enter numbers!");
+                System.out.println("You should enter numbers 1-3!");
             }
 
         }
